ec1ed8f8c6bf897344cddeb318a21f10926a319a
Berman, Tomer <tomer_berman@bmc.com>
2023-05-31 22:57:23 +0300

    IF-111

--- main.tf
+++ main.tf
@@ -21,3 +21,8 @@ resource "aws_instance" "app_server" {
     Name = "AppServerInstanceCLOUD"
   }
 }
+
+resource "aws_ec2_instance_state" "test" {
+  instance_id = aws_instance.app_server.id
+  state       = "running"
+}
