a1d8074e301e817926269b9bd5ce4b565e8f431d
Berman, Tomer <tomer_berman@bmc.com>
2023-05-31 22:04:23 +0300

    IF-111

---
+++ main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0e1c5be2aa956338b"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
+
+resource "aws_ec2_instance_state" "test" {
+  instance_id = aws_instance.app_server.id
+  state       = "stopped"
+}
